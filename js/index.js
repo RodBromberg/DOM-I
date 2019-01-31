715b5c1fe1adfa9530f07b115cb17bdf12c094e4
RodBromberg <rod.bromberg@yahoo.com>
2019-01-30 21:21:14 -0500

    commit

--- js/index.js
+++ js/index.js
@@ -40,3 +40,67 @@ const siteContent = {
 // Example: Update the img src for the logosdsd
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+
+// const aElements = document.querySelectorAll('a')
+// let count = 0
+// aElements.forEach(elem => {
+//     count++
+//     let value = Object.values(siteContent.nav)[count]
+//     aElements[count].textContent = value
+// })
+
+
+
+const aElements = document.querySelectorAll('a')
+
+for (let i = 0; i < aElements.length; i++) {
+    let value = Object.values(siteContent.nav)[i]
+    aElements[i].textContent = value
+}
+
+const h1_tag = document.querySelector('.cta-text h1')
+h1_tag.textContent = siteContent.cta['h1']
+
+
+const button = document.querySelector('.cta-text button')
+button.textContent = siteContent.cta['button']
+
+
+const imgHeader = document.getElementById('cta-img')
+imgHeader.src = siteContent.cta["img-src"]
+
+const text_section = document.querySelectorAll('.text-content h4')
+
+text_section[0].textContent = siteContent["main-content"]["features-h4"]
+text_section[1].textContent = siteContent["main-content"]["about-h4"]
+text_section[2].textContent = siteContent["main-content"]["services-h4"]
+text_section[3].textContent = siteContent["main-content"]["product-h4"]
+text_section[4].textContent = siteContent["main-content"]["vision-h4"]
+
+const features_content = document.querySelector('.text-content p')
+features_content.textContent = siteContent["main-content"]["features-content"]
+
+
+const about_Content = document.querySelectorAll('.text-content p')[1]
+about_Content.textContent = siteContent["main-content"]["about-content"]
+
+
+
+//fdfdgdfgfgddfg
+
+
+// main-content": {
+//   "features-h4": "Features",
+//   "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+//   "about-h4": "About",
+//   "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+//   "middle-img-src": "img/mid-page-accent.jpg",
+//   "services-h4": "Services",
+//   "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+//   "product-h4": "Product",
+//   "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+//   "vision-h4": "Vision",
+//   "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+// },
